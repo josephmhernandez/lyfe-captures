495b6c800a5b230e36ba4bcb50839fbb684cb443
Joseph Hernandez <joseph@josephs-air.myfiosgateway.com>
2022-08-07 23:30:42 -0400

    shadow on nav bar

--- lyfe-captures/components/layout/Banner.js
+++ lyfe-captures/components/layout/Banner.js
@@ -1,31 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Fragment } from "react";
 import logoPic from "../../public/logo-small.png";
 import cartPic from "../../public/cart-icon-small.png";
+import classes from "./NavigationBar.module.css";
+//Navigation Bar
 
 const Banner = (props) => {
   const DUMMY_TEXT = "UP TO 30% OFF CUSTOM MAP PRINTS WITH CODE 'SUMMER'";
   return (
     // Promotion text
     // Logo Link
-    <Fragment>
-      <Link href="/">
-        <a>
-          <Image src={logoPic} alt="Lyfe Captures" />
-        </a>
-      </Link>
-      <div className="promotion-text">
-        <p>{DUMMY_TEXT}</p>
-      </div>
-      <div className="cart-icon">
-        <Link href="/cart">
-          <a>
-            <Image src={cartPic} alt="CART" />
-          </a>
+    <div className={classes.topnav}>
+      <nav className={classes.topnav}>
+        <Link href="/" LyfeCaptures>
+          LyfeCaptures
         </Link>
-      </div>
-    </Fragment>
+        <div className={classes.cartNav}>
+          <Link href="/cart">
+            <a>
+              <Image src={cartPic} alt="CART" />
+            </a>
+          </Link>
+        </div>
+      </nav>
+      {props.children}
+    </div>
   );
 };
 
--- lyfe-captures/components/product/ProductPicture.js
+++ lyfe-captures/components/product/ProductPicture.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import classes from './ProductPicture.module.css';
 
 const ProductPicture = (props) => {
   return (
-    <div className="product-picture">
+    <div className={classes.productImage}>
       <Image src={props.src} alt={props.alt}/>
     </div>
   );
