4218a3ce2c80a2ddcdf96c1ba719e023401e88b9
Joseph Hernandez <joseph@josephs-air.myfiosgateway.com>
2022-08-11 23:41:44 -0400

    add font

--- lyfe-captures/pages/maps/index.js
+++ lyfe-captures/pages/maps/index.js
@@ -2,11 +2,9 @@ import Layout from "../../components/layout/Layout";
 
 const MapPage = () => {
   return (
-    <Layout hideFooter={true}>
-      <div>
-        <h1>Map Page</h1>
-      </div>
-    </Layout>
+    <div>
+      <h1>Map Page</h1>
+    </div>
   );
 };
 export default MapPage;
--- lyfe-captures/pages/sizes/index.js
+++ lyfe-captures/pages/sizes/index.js
@@ -1,12 +1,8 @@
-import Layout from "../../components/layout/Layout";
-
 const Sizes = () => {
   return (
-    <Layout>
-      <div>
-        <h1>Sizes</h1>
-      </div>
-    </Layout>
+    <div>
+      <h1>Sizes</h1>
+    </div>
   );
 };
 export default Sizes;
