a2b92cd49036918c3ad9d83bae08e965c56309ea
Joseph Hernandez <joseph@josephs-air.myfiosgateway.com>
2022-08-14 22:26:49 -0400

    fix font api warning

--- lyfe-captures/pages/index.js
+++ lyfe-captures/pages/index.js
@@ -1,28 +1,12 @@
-import Head from "next/head";
+
 import { Fragment } from "react";
 import Layout from "../components/layout/Layout";
 import MapsLandingPage from "../components/product/maps/MapsLandingPage";
-import styles from "../styles/Home.module.css";
+
 
 export default function Home() {
   return (
     <Fragment>
-      {/* <Head>
-        <title>Lyfe Captures</title>
-        <meta
-          name="description"
-          content="Create a personalized gift worth giving. Capture memories and moments forever."
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" href="/play-button.ico" />
-      </Head> */}
-      
-
       <MapsLandingPage />
     </Fragment>
   );
