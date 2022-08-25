c9d3c40bc0d247460834f6ed0342d1178d3890dd
Joseph Hernandez <joseph@josephs-air.myfiosgateway.com>
2022-08-24 20:07:07 -0400

    clean up accordion. add color to radio buttons. remove size accordion

--- lyfe-captures/components/createMap/CreateMap.js
+++ lyfe-captures/components/createMap/CreateMap.js
@@ -67,26 +67,3 @@ ACCORDION COMPONENT
   );
 };
 export default CreateMap;
-
-
-
-
-
-// <div className={classes.accordionBox}>
-// <Layout className={classes.accordion}>
-//   <Accordion >
-//     <AccordionItem uniqueKey={1} title="head 1">
-//       Hello 1Hello 1Hello 1Hello 1
-//     </AccordionItem>
-//     <AccordionItem uniqueKey={2} title="head 2">
-//       Hello 2Hello 2Hello 2Hello 2
-//     </AccordionItem>
-//     <AccordionItem uniqueKey={3} title="head 3">
-//       <p> AYOOOOO</p>
-//     </AccordionItem>
-//   </Accordion>
-// </Layout>
-// </div>
-
-
-
--- lyfe-captures/components/createMap/StyleAccordion/Orientation.js
+++ lyfe-captures/components/createMap/StyleAccordion/Orientation.js
@@ -6,30 +6,49 @@ import {
   Radio,
 } from "@mui/material";
 
+import classes from "./Orientation.module.css";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: publicRuntimeConfig.primaryColor,
+    },
+    secondary: {
+      main: publicRuntimeConfig.primaryColor,
+    },
+  },
+});
+
 const Orientation = () => {
   return (
-    <FormControl>
-      <FormLabel id="orientation-radio-buttons-group-custom-map">
-        Orientation
-      </FormLabel>
-      <RadioGroup
-        row
-        aria-labelledby="orietntation-radio-buttons-group-custom-map"
-        defaultValue="portrait"
-        name="orientation-radio-buttons-group"
-      >
-        <FormControlLabel
-          value="portrait"
-          control={<Radio />}
-          label="Portrait"
-        />
-        <FormControlLabel
-          value="landscape"
-          control={<Radio />}
-          label="Landscape"
-        />
-      </RadioGroup>
-    </FormControl>
+    <div className={classes.container}>
+      <ThemeProvider theme={theme}>
+        <FormControl>
+          <RadioGroup
+            row
+            aria-labelledby="orietntation-radio-buttons-group-custom-map"
+            defaultValue="portrait"
+            name="orientation-radio-buttons-group"
+          >
+            <FormControlLabel
+              value="portrait"
+              control={<Radio color="primary" />}
+              label="Portrait"
+            />
+            <FormControlLabel
+              value="landscape"
+              control={<Radio color="primary" />}
+              label="Landscape"
+            />
+          </RadioGroup>
+        </FormControl>
+      </ThemeProvider>
+    </div>
   );
 };
 export default Orientation;
