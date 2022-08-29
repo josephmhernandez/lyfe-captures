ae266074d2274a61fbffa64c5c26a9a6903fa4a2
Joseph Hernandez <joseph@josephs-air.myfiosgateway.com>
2022-08-28 21:16:34 -0400

    add dynamic text that's centered in space on poster

--- lyfe-captures/components/createMap/CardOverlay.js
+++ lyfe-captures/components/createMap/CardOverlay.js
@@ -1,14 +1,62 @@
 import { Card, collapseClasses, Paper, Typography } from "@mui/material";
 import classes from "./CreateMap.module.css";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const CardOverlay = (props) => {
+  const textPrimary = useSelector((state) => state.map.textPrimary);
+  const textSecondary = useSelector((state) => state.map.textSecondary);
+  const textCoordinates = useSelector((state) => state.map.textCoordinates);
+  const [showTextPrimary, setShowTextPrimary] = useState(true);
+  const [showTextSecondary, setShowTextSecondary] = useState(true);
+  const [showTextCoordinates, setShowTextCoordinates] = useState(true);
+  const styleTextPrimary = {
+    position: "relative",
+    fontFamily: "Semplicita",
+    fontSize: "1.5rem",
+    color: "black",
+    padding: "-1.5rem",
+  };
+  const styleTextSecondary = {};
+  const styleTextCoordinates = {};
+
+  useEffect(() => {
+    if (textPrimary) {
+      setShowTextPrimary(true);
+    } else {
+      setShowTextPrimary(false);
+    }
+
+    if (textSecondary) {
+      setShowTextSecondary(true);
+    } else {
+      setShowTextSecondary(false);
+    }
+
+    if (textCoordinates) {
+      setShowTextCoordinates(true);
+    } else {
+      setShowTextCoordinates(false);
+    }
+  }, [textPrimary, textSecondary, textCoordinates]);
+
   return (
     <Paper className={classes.paper} elevation={24}>
       <div className={classes.homeMap}>{props.children}</div>
       <div className={classes.textMap}>
-        <Typography className={classes.textPrimary}>AUSTIN, TEXAS</Typography>
-        <Typography className={classes.textSecondary}></Typography>
-        <Typography className={classes.textCoordinates}>N40 23'37.234" W111 54'54.73"</Typography>
+        {showTextPrimary && (
+          <Typography className={classes.textPrimary}>{textPrimary}</Typography>
+        )}
+        {showTextSecondary && (
+          <Typography className={classes.textSecondary}>
+            {textSecondary}
+          </Typography>
+        )}
+        {showTextCoordinates && (
+          <Typography className={classes.textCoordinates}>
+            {textCoordinates}
+          </Typography>
+        )}
       </div>
     </Paper>
   );
