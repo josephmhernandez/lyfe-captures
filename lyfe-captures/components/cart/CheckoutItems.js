2393aa2b137ced2640c45fe22f9ec3b7220623a1
Joseph Hernandez <joseph@josephs-air.myfiosgateway.com>
2022-10-26 20:11:02 -0400

    image on cart & cart fomat

--- lyfe-captures/components/cart/CheckoutItems.js
+++ lyfe-captures/components/cart/CheckoutItems.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import { ClassNames } from "@emotion/react";
+import React from "react";
 // import { Item, Header } from 'semantic-ui-react';
-
+import classes from "./CheckoutItems.module.css";
 const CheckoutItems = (props) => {
-    // console.log(props, 'props from checkout container')
+  console.log(props, "props from checkout container");
 
+  console.log(props.item);
+  const item = props.item;
+  console.log(item.image.url);
+  const total_price = props.item.line_total.formatted_with_symbol;
+  const quantity = "todo x1";
+  const identify_map = "todo White";
 
-    return (
-        <>
-            <div className='item-group-name-size'>
-                <h3>{props.item.product_name}</h3>
-                {/* TO DO: fix the variants on the cart selection */}
-                {/* <span>{props.item?.variants[0].option_name}</span> */}
-            </div>
-            <h4>{props.item.line_total.formatted_with_symbol}</h4>
-        </>
-    );
+  return (
+    <>
+      <div className={classes.itemCart}>
+        <img src={item.image.url} />
+        <div className={classes.productNameDescription}>
+          <h3>{props.item.product_name}</h3>
+          <p>{identify_map}</p>
+        </div>
+        <p>{quantity}</p>
+        <p>{total_price}</p>
+      </div>
+    </>
+  );
 };
 
 export default CheckoutItems;
