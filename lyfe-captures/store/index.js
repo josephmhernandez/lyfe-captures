b3907e500583d17797404f283fd1bb20be5bb5ea
Joseph Hernandez <joseph@josephs-air.myfiosgateway.com>
2022-11-15 18:31:10 -0500

    quick fix

--- lyfe-captures/store/index.js
+++ lyfe-captures/store/index.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import mapSlice from "./map-slice";
-import cartSlice from "./cart-slice";
 
 const store = configureStore({
   reducer: { map: mapSlice.reducer },
