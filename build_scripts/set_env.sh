dab7d8d6ea241f0b7570fab6c3995c95b7b3ced9
Joseph Hernandez <josephmhernandez15@gmail.com>
2023-01-28 10:24:49 -0500

    put env file in root folder

--- build_scripts/set_env.sh
+++ build_scripts/set_env.sh
@@ -1,23 +1,25 @@
+# Don't run this locally - it's for CI/CD only. It will override your local .env file. 
 # Write Secrets (as env vars)
-echo "BUILD_ENV=$BUILD_ENV" >> .env
+ENV_PATH=../.env
+echo "BUILD_ENV=$BUILD_ENV" >> $ENV_PATH
 echo "Building for " $BUILD_ENV "....."
-echo "CHEC_PK=$CHEC_PK" >> .env
-echo "STRIPE_PK=$STRIPE_PK" >> .env
-echo "MAPTILER_API_KEY=$MAPTILER_API_KEY" >> .env
-echo "GOOGLE_API_KEY=$GOOGLE_API_KEY" >> .env
+echo "CHEC_PK=$CHEC_PK" >> $ENV_PATH
+echo "STRIPE_PK=$STRIPE_PK" >> $ENV_PATH
+echo "MAPTILER_API_KEY=$MAPTILER_API_KEY" >> $ENV_PATH
+echo "GOOGLE_API_KEY=$GOOGLE_API_KEY" >> $ENV_PATH
 
 # Wrtie Constants (as env vars)
-echo "MAX_CHARS_PRIMARY=50" >> .env
-echo "MAX_CHARS_SECONDARY=60" >> .env
-echo "MAX_PINS=1" >> .env
-echo "MAP_ZOOM=13" >> .env
-echo "MAP_LOCATION_LAT=40.730610" >> .env
-echo "MAP_LOCATION_LNG=-73.935242" >> .env
-echo "INITIAL_PIN_SIZE=50" >> .env
-echo "MAP_LOCATION=" >> .env
-echo "MAX_MAP_ZOOM=18" >> .env
-echo "MIN_MAP_ZOOM=4" >> .env
-echo "TILE_ZOOM_OFFSET=2" >> .env
-echo "CART_ITEM_MAX_QUANTITY=50" >> .env
-echo "EMAIL_SUPPORT=help@mapyourmemory.com" >> .env
+echo "MAX_CHARS_PRIMARY=50" >> $ENV_PATH
+echo "MAX_CHARS_SECONDARY=60" >> $ENV_PATH
+echo "MAX_PINS=1" >> $ENV_PATH
+echo "MAP_ZOOM=13" >> $ENV_PATH
+echo "MAP_LOCATION_LAT=40.730610" >> $ENV_PATH
+echo "MAP_LOCATION_LNG=-73.935242" >> $ENV_PATH
+echo "INITIAL_PIN_SIZE=50" >> $ENV_PATH
+echo "MAP_LOCATION=" >> $ENV_PATH
+echo "MAX_MAP_ZOOM=18" >> $ENV_PATH
+echo "MIN_MAP_ZOOM=4" >> $ENV_PATH
+echo "TILE_ZOOM_OFFSET=2" >> $ENV_PATH
+echo "CART_ITEM_MAX_QUANTITY=50" >> $ENV_PATH
+echo "EMAIL_SUPPORT=help@mapyourmemory.com" >> $ENV_PATH
 
