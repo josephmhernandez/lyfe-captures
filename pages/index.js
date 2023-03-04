c39d508984893c2d494d5b2528e0cb108b5517de
Joseph Hernandez <josephmhernandez15@gmail.com>
2023-03-04 13:29:53 -0600

    clean up code

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import Layout from "../components/layout/Layout";
 import MapsLandingPage from "../components/product/maps/MapsLandingPage";
 import MapsLandingPageMobile from "../components/product/maps/MapsLandingPageMobile";
 export default function Home(props) {
-  console.log("props", props);
-
   if (props.isMobileView) {
     return (
       <Fragment>
@@ -25,7 +23,6 @@ Home.Layout = Layout;
 // Need this here to get the current device that the user is on.
 // Can't have getInitialProps in the component itself. It must be on the page component.
 Home.getInitialProps = async (ctx) => {
-  console.log("getInitialProps", ctx);
   let isMobileView = (
     ctx.req ? ctx.req.headers["user-agent"] : navigator.userAgent
   ).match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i);
